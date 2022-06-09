bc8dacdd91a94759dae52cdb53cf54cd98ff76cc
dcunhamaciel <dcunhamaciel@gmail.com>
2022-06-09 20:23:12 -0300

    Model People

---
+++ database/migrations/2022_06_09_230822_create_people_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('people', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 60);
+            $table->integer('height');
+            $table->integer('mass');	
+            $table->string('hair_color', 60);
+            $table->string('skin_color', 60);
+            $table->string('eye_color', 60);
+            $table->string('birth_year', 60);
+            $table->string('gender', 60);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('people');
+    }
+};
