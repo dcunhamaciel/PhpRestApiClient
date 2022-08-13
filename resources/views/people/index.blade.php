37c168937abd0a8ba45f68b99f7c4cc0477acf1d
dcunhamaciel <dcunhamaciel@gmail.com>
2022-08-13 10:36:33 -0300

    Bootstrap

--- resources/views/people/index.blade.php
+++ resources/views/people/index.blade.php
@@ -1,16 +1,20 @@
 <!DOCTYPE html>
 <html lang="pt-br">
-    <head>
-        <title>People</title>
+    <head>        
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.css" integrity="sha512-h1rwF0uB6r2IuEZcyjPrR53bBKN9Wb4yL+w3Rdlzmc3CkBF1gMSFvQIIstnu4bEtYDaKca5ke5S8UBAACRImyg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+        <title>People</title>
     </head>
 
     <body>
         <div>
-            <a href="{{ route('people.update') }}"><button>Update People</button></a>            
-            <hr></hr>
-            <table border="1" width="100%">
-                <thead>
+            <a href="{{ route('people.update') }}">
+                <button class="btn btn-outline-primary">Update People</button>
+            </a>            
+            <table border="1" width="100%" class="table table-striped table-hover">
+                <thead class="table-dark">
                     <tr>
                         <th>Name</th>
                         <th>Height</th>
